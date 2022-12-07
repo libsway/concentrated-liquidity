5e8194f4c149509b5b27b687fdc6d1011cc18646
alphak3y <alphak3y@protonmail.com>
2022-12-06 21:56:20 -0700

    harness test; sq63x64 updates

--- tests/harness.rs
+++ tests/harness.rs
@@ -1,10 +1,4 @@
 use fuels::prelude::*;
-use std::net::SocketAddr;
-use std::net::IpAddr;
-use std::net::Ipv4Addr;
-use fuels::test_helpers::launch_custom_provider_and_get_wallets;
-use fuel_chain_config::ChainConfig;
-use fuels::fuel_node::Config;
 
 abigen!(
     ExeguttorTests,
@@ -13,88 +7,7 @@ abigen!(
 
 #[tokio::test]
 async fn sq63x64() {
-    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 4000);
-    let wallet = launch_custom_provider_and_get_wallets(
-        WalletsConfig::new(
-            Some(1),             /* Single wallet */
-            Some(1),             /* Single coin (UTXO) */
-            Some(1_000_000_000), /* Amount per coin */
-        ),
-        Config::new(
-            socket,
-            false,
-            false,
-            false,
-            false
-        ),
-        ChainConfig::new(
-            "cl_libs_test",
-            BlockProduction.ProofOfAuthority,
-            1_000_000_000, // gas limit
-            None,
-            ConsensusParameters::DEFAULT.with_max_gas_per_tx(10000000000000).with_gas_per_byte(0),
-        )
-    ).await;
-
-    let (contract_instance, _id) = get_test_contract_instance(wallet).await;
-
-    let result = contract_instance.methods()
-        .test_most_sig_bit_idx()
-            .call()
-            .await
-            .unwrap()
-            .value;
-
-    println!("{}", result);
-
-    let result = contract_instance.methods()
-        .test_binary_log()
-            .call()
-            .await
-            .unwrap()
-            .value;
-
-    println!("{}", result.value.upper);
-    println!("{}", result.value.lower);
-    let base: u128 = 2;
-    println!("{}", base.pow(64));
-
-    let result = contract_instance.methods()
-        .test_abs_u128()
-            .call()
-            .await
-            .unwrap()
-            .value;
-
-    println!("{}", result.upper);
-
-    assert!(result != result);
-}
-
-#[tokio::test]
-async fn sq63x64() {
-    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 4000);
-    let wallet = launch_custom_provider_and_get_wallets(
-        WalletsConfig::new(
-            Some(1),             /* Single wallet */
-            Some(1),             /* Single coin (UTXO) */
-            Some(1_000_000_000), /* Amount per coin */
-        ),
-        Config::new(
-            socket,
-            false,
-            false,
-            false,
-            false
-        ),
-        ChainConfig::new(
-            "cl_libs_test",
-            BlockProduction.ProofOfAuthority,
-            1_000_000_000, // gas limit
-            None,
-            ConsensusParameters::DEFAULT.with_max_gas_per_tx(10000000000000).with_gas_per_byte(0),
-        )
-    ).await;
+    let wallet = launch_provider_and_get_wallet().await;
 
     let (contract_instance, _id) = get_test_contract_instance(wallet).await;
 
